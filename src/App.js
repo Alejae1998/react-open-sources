0bffe0887573fa5258fd38ba74aae424cb47414b
Alejae1998 <alejagr1998@gmail.com>
2023-01-31 15:47:04 -0800

    addint material ui button to the  links

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import { Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Charts from './components/Charts/Charts.js';
 import Table from './components/Table/Table.js';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/table">Table</Link>
-        <Link to="/charts">Charts</Link>
+        {/* <Link to="/table">Table</Link> */}
+        <Button href="/table" varian="contained">
+          Table
+        </Button>
+        {/* <Link to="/charts">Charts</Link> */}
+        <Button href="/charts" varian="contained">
+          Chart
+        </Button>
       </nav>
       <Switch>
         <Route path="/table" component={Table} />
